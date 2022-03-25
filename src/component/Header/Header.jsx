b1c13b6d52efe47eaa71fda5270f2c1be621448a
niiyazobug <niiyaz5555@gmail.com>
2022-03-25 22:02:07 +0600

    header file added

---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+    return (
+        <div>
+            <h2>hey</h2>
+        </div>
+    );
+};
+
+export default Header;
