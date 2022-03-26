8d658da2b7e17056c3a477664e9c1703a4c324de
niiyazobug <niiyaz5555@gmail.com>
2022-03-26 15:10:37 +0600

    according added

--- src/component/Cart/Cart.jsx
+++ src/component/Cart/Cart.jsx
@@ -3,29 +3,33 @@ import './Cart.css'
 
 const Cart = ({ cart }) => {
 
+    const emptyCart = () => {
+
+    }
+
     return (
-        
-            <div className=''>
-                <h3>Order Summary</h3>
-                <div className='info-container'>
 
-                    {
-                        cart.map((product) => (
-                            <div className='product-info-in-cart' key={product.id}>
-                                <img src={product.img} alt="" />
-                                <p>{product.name}</p>
-                            </div>
-                        ))
-                    }
-                </div>
-                <button>Click to random Select</button> 
-                <br /> <br />
-               <button>Select Again</button>
+        <div className='cart'>
+            <h3>Order Summary</h3>
+            <div className='info-container'>
+
+                {
+                    cart.map((product) => (
+                        <div className='product-info-in-cart' key={product.id}>
+                            <img src={product.img} alt="" />
+                            <p>{product.name}</p>
+                        </div>
+                    ))
+                }
             </div>
+            <button>Click To Random Select</button>
+            <br />
+            <button>Select Again</button>
+        </div>
 
-            
 
-        
+
+
     );
 };
 
